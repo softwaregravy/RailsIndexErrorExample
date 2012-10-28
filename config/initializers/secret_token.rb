330a7102cf9eeceb33417033e1350ed7e35eb6a9
John Hinnegan <john.hinnegan@gmail.com>
2012-10-28 14:52:55 -0700

    Initial create

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ReproIndexError::Application.config.secret_token = 'a854762aba0dff5558e96f794046677a03aec10436c0fd9ce23e080e1b63f08f130db5116e9c4a2d70bb123a5446e8487c38916d9f40711e52fad4b7a5c42842'
